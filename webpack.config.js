7c2dc7a6806d97dda25ac97419c800c2a48827ff
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-25 18:17:26 +0200

    The most basic webpack (with webpack dev server) setup.

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  }
+};
