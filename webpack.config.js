b5a3add39a0a6a82487464ea1ce74ad2ef896b1e
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-25 19:10:35 +0200

    Configured postcss to use autoprefixer and to resolve nested selectors.

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,16 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('autoprefixer'),
+                require('postcss-nested')
+              ]
+            }
+          }
         ]
       }
     ]
