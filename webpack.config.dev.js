8f16dab654a5d571107504ae911863d3ac628262
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-28 15:50:51 +0200

    Webpack config for development.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,6 @@ var path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
     publicPath: '/assets/'
   },
